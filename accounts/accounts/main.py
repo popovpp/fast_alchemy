659ccec26161fd6823ef06f987a0a00d6cd9f94b
Pavel <popovpp@yandex.ru>
2022-02-13 23:25:40 +0300

    Added fastapi-users, not end yet

--- accounts/accounts/main.py
+++ accounts/accounts/main.py
@@ -1,4 +1,18 @@
 from fastapi import FastAPI
+from fastapi_users import FastAPIUsers
+
+from .user_manager import get_user_manager
+
+
+
+fastapi_users = FastAPIUsers(
+    get_user_manager,
+    [auth_backend],
+    User,
+    UserCreate,
+    UserUpdate,
+    UserDB,
+)
 
 app = FastAPI()
 
@@ -6,4 +20,3 @@ app = FastAPI()
 @app.get("/")
 def index():
     return {"message": "Hello world!"}
-    
--- app/app/db.py
+++ app/app/db.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import sessionmaker
 from .config import settings
 
 engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 @as_declarative()
